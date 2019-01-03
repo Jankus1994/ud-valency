0f958ca107b50a225d1ed6921eb9976c633d522c
Jan Faryad <faryadj@u1-10.ms.mff.cuni.cz>
2019-01-03 18:36:39 +0100

    renaming docs to data

--- valency/scripts/frame_extraction.sh
+++ valency/scripts/frame_extraction.sh
@@ -3,5 +3,5 @@
 export PATH=../../udapi-python/bin:$PATH
 export PYTHONPATH=../../udapi-python/:$PYTHONPATH
 
-udapy read.Conllu files=../docs/cs_pdt-ud-concat.conllu valency.Frame_extractor > ../docs/cs_output.txt
-udapy read.Conllu files=../docs/fi_tdt-ud-concat.conllu valency.Frame_extractor > ../docs/fi_output.txt
+udapy read.Conllu files=../data/cs_pdt-ud-concat.conllu valency.Frame_extractor > ../data/cs_output.txt
+udapy read.Conllu files=../data/fi_tdt-ud-concat.conllu valency.Frame_extractor > ../data/fi_output.txt

5383f8c644185446c54a0ae1b900e713be42b3c0
Jan Faryad <faryadj@u-pl19.ms.mff.cuni.cz>
2020-07-10 14:07:56 +0200

    new extraction sh

--- valency/scripts/frame_extraction.sh
+++ valency/scripts/frame_extraction.sh
@@ -3,6 +3,8 @@
 export PATH=../../udapi-python/bin:$PATH
 export PYTHONPATH=../../udapi-python/:$PYTHONPATH
 
-udapy read.Conllu files=../data/cs_pdt-ud-concat.conllu valency.Frame_extractor > ../data/cs_output.html
-udapy read.Conllu files=../data/fi_tdt-ud-concat.conllu valency.Frame_extractor > ../data/fi_output.html
-udapy read.Conllu files=../data/en_lines-ud-concat.conllu valency.Frame_extractor > ../data/en_output.html
+#udapy read.Conllu files=../data/cs_en_pokus.conllu valency.Cs_En_frame_aligner align_file_name=../data/cs_en_pokus.cs_ali output=../data/cs_en_pokus.pic
+#python3 html_creator.py ../data/cs_en_pokus.pic > ../data/cs_en_pokus.html
+
+udapy read.Conllu files=../data/cs_en_merged/cs_en_1_1_merged.conllu valency.Cs_En_frame_aligner align_file_name=../data/cs_en_align/cs-en_1_1.ali output=../data/cs_en_merged/cs_en_1_1.pic
+python3 html_creator.py ../data/cs_en_merged/cs_en_1_1.pic > ../data/cs_en_1_1.html

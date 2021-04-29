c91d2968ae062f4b0e47ca9d0600e6905c3d2db2
Jan Faryad <faryadj@aic.aic.hide.ms.mff.cuni.cz>
2021-04-29 17:01:24 +0200

    cleanup

--- valency/scripts/vallex_comparer.sh
+++
@@ -1,4 +0,0 @@
-export PATH=../../udapi-python/bin:$PATH
-export PYTHONPATH=../../udapi-python/:$PYTHONPATH
-
-python3 vallex_comparer.py ../data/cs_en_pokus.pic ../data/vallex_records.pic
--- valency/scripts/vallex_extractor.sh
+++
@@ -1,1 +0,0 @@
-python3 vallex_extractor.py ../data/vallex_3.0.xml ../data/vallex_records.pic

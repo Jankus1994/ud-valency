c981dc70eb766bd9de967eec580a1a667ca611ef
Jankus1994 <faryadj@seznam.cz>
2024-01-09 17:40:09 +0100

    folders

---
+++ data/sents/acquis_cs_en_100.cs
@@ -0,0 +1,100 @@
+Dodatková dohoda k Dohodě o výrobcích hodinářského průmyslu mezi Evropským hospodářským společenstvím a jeho členskými státy a Švýcarskou konfederací [1]
+EVROPSKÉ HOSPODÁŘSKÉ SPOLEČENSTVÍ
+na jedné straně a
+ŠVÝCARSKÁ SPOLKOVÁ RADA
+na straně druhé,
+VZHLEDEM K TOMU, ŽE dne 30. června 1967 byla v Ženevě podepsána Dohoda o výrobcích hodinářského průmyslu mezi Evropským hospodářským společenstvím a jeho členskými státy a Švýcarskou konfederací;
+VZHLEDEM K TOMU, ŽE pro řádné fungování této dohody je nezbytné, aby byla přijata doplňující ustanovení;
+VZHLEDEM K TOMU, ŽE používání označení "švýcarské" pro hodinky se řídí nařízením Švýcarské spolkové rady ze dne 23. prosince 1971;
+VZHLEDEM K TOMU, ŽE v hodinářském průmyslu existuje úzká spolupráce mezi Evropským hospodářským společenstvím a Švýcarskem;
+BEROUCE V ÚVAHU zrušení racionalizačních grantů sjednaných ze strany Ebauches SA a ASUAG současně se vstupem této dohody v platnost a souběžné zrušení kvóty uvedené v bodu B 3 b) výše uvedené Dohody,
+SE DOHODLY takto:
+Článek 1
+Snížení celních sazeb uvedená v článcích 1 a 5 Dohody ze dne 30. června 1967 vstupují v platnost současně s touto dohodou.
+Článek 2
+Aby hodinky se strojkem švýcarské výroby, pokud jde nejméně o 50 % hodnoty všech částí a součástí, včetně nákladů na montáž, mohly být považovány za švýcarské podle čl. 2 odst. 2, písm. b) nařízení Švýcarské spolkové rady ze dne 23. prosince 1971, kterým se řídí označení "švýcarské" pro hodinky, zavádí se postup pro vydávání osvědčení mezi Švýcarskem a Společenstvím v souladu s těmito pravidly:
+1. Polotovary hodinkových strojků, vyrobené ve Společenství a uvedené v seznamu v příloze této dohody, a seřizovací ústrojí a jiné základní součásti hodinkových strojků, vyrobené ve Společenství a doplňující polotovary hodinkových strojků vyrobené ve Švýcarsku nebo ve Společenství, se považují svou kvalitou za rovnocenné polotovarům a součástem švýcarské výroby se srovnatelnými technickými vlastnostmi. Rozumí se, že hodinky a hodinkové strojky sestavené z těchto polotovarů a součástí musejí vyhovovat požadavkům zákonem stanovené technické kontroly ve Švýcarsku.
+2. Seznam polotovarů hodinových strojků vyrobených ve Společenství uvedený v odstavci 1 tohoto článku se pravidelně aktualizuje tímto způsobem:
+a) Žádosti o zápis nových kalibrů polotovarů do tohoto seznamu zasílají Švýcarské hodinářské komoře sdružení výrobců hodin a hodinek ve Společenství nebo výrobci polotovarů hodinových strojků usazení ve Společenství. Žádosti doprovází technický popis kalibrů polotovarů hodinových strojků. Švýcarská hodinářská komora nový kalibr nebo nové kalibry bezodkladně zapisuje do uvedeného seznamu.
+Pokud by byl požadován zápis kalibru polotovarů, který se již ve Švýcarsku používal a neprošel ve Švýcarsku zákonem stanovenou kontrolou, Švýcarská hodinářská komora může zápis zamítnout. V tomto případě se může žadatel uchýlit k postupu uvedenému v odstavci 3 tohoto článku.
+b) Odnětí kalibrů polotovarů hodinových strojků zapsaných na seznamu sdělují Švýcarské hodinářské komoře sdružení výrobců hodin a hodinek ve Společenství nebo výrobci, kteří předtím žádali o zápis těchto kalibrů do seznamu.
+c) Pokud by nebyly splněny požadavky zákonem stanovené technické kontroly, Švýcarská hodinářská komora může žádat, aby příslušné kalibry polotovarů hodinových strojků byly vyškrtnuty ze seznamu, a tento záměr oznamuje příslušné straně. V případě neshody se příslušná strana může do dvou měsíců uchýlit k postupu uvedenému v odstavci 3 tohoto článku.
+d) Všechny žádosti o změnu tohoto seznamu podle výše uvedených pododstavců a), b) a c) Švýcarská hodinářská komora bez prodlení oznamuje Smíšenému výboru.
+3. V případě sporu o rovnocennost kvality kterákoli zúčastněná strana záležitost okamžitě předloží Smíšenému výboru zřízenému podle článku 9 dohody ze dne 30. června 1967.
+První strana, která se tak rozhodne, ze své vlastní iniciativy požádá o společné odborné stanovisko institutu pro oficiální kontrolu kvality ve švýcarském hodinářském průmyslu a odpovídajícího kvalifikovaného institutu ve Společenství, navrženého zúčastněnou stranou Společenství.
+Instituty mají tříměsíční lhůtu na předložení požadovaného odborného stanoviska.
+Za účelem předložení tohoto odborného stanoviska se instituty mezi sebou dohodnou na odběru dvou dostatečně reprezentativních sérií hodinek nebo hodinových strojků, z nichž každá obsahuje nejvýše 50 hodinek nebo hodinových strojků, přičemž jedna bude ze švýcarského podniku a druhá z podniku Společenství.
+Po vyzkoušení těchto sérií v souladu se zákonnými standardy technické kontroly platnými ve Švýcarsku instituty porovnají své výsledky a společně vypracují zprávu pro smíšený výbor, obsahující jejich závěry a případné návrhy.
+Smíšený výbor tuto zprávu posoudí na svém nejbližším zasedání.
+Článek 3
+Dohoda a seznam v příloze se zveřejňují v úředních věstnících smluvních stran a příslušná obchodní sdružení je oznamují výrobcům hodin a hodinek.
+Změny seznamu v příloze se zveřejňují a oznamují stejným způsobem.
+Nejméně každé tři roky počínaje vstupem Dohody v platnost Smíšený výbor seznam aktualizuje, aby zohlednil všechny zápisy nebo výmazy které v něm byly v dané době provedeny. Nový seznam se zveřejňuje a oznamuje stejným způsobem. Jakékoli další informace je možno získat od příslušných obchodních sdružení.
+Článek 4 Kterákoli smluvní strana může dohodu vypovědět oznámením s dvanáctiměsíční výpovědní lhůtou.
+Článek 5
+Dohodu smluvní strany uzavírají a ratifikují v souladu s jejich vlastními postupy.
+Dohoda vstupuje v platnost dnem 1. ledna 1973 za předpokladu, že k tomuto dni si smluvní strany vymění své ratifikační listiny.
+Pokud by ratifikační listiny byly vyměněny mezi 1. lednem a 30. listopadem 1973, dohoda vstupuje v platnost prvním dnem druhého měsíce následujícího po této výměně.
+[1] Dohoda je sepsána pouze ve francouzském jazyce. --------------------------------------------------
+Dohoda
+mezi Evropským hospodářským společenstvím a Švýcarskou konfederací
+EVROPSKÉ HOSPODÁŘSKÉ SPOLEČENSTVÍ
+na jedné straně a
+ŠVÝCARSKÁ KONFEDERACE
+na straně druhé,
+PŘEJÍCE SI upevnit a rozšířit, v důsledku rozšíření Evropského hospodářského společenství, hospodářské vztahy mezi Společenstvím a Švýcarskem a zajistit s náležitým ohledem na poctivé podmínky hospodářské soutěže harmonický rozvoj obchodu mezi stranami jako příspěvek k budování Evropy,
+ODHODLÁNY za tímto účelem postupně omezit překážky obchodu v souladu s ustanoveními Všeobecné dohody o clech a obchodu s ohledem na budování oblastí volného obchodu,
+VYHLAŠUJÍCE svoji připravenost zvážit na základě všech souvisejících hledisek, a zejména s ohledem na vývoj ve Společenství, možnost rozvoje a prohlubování vzájemných vztahů, bude-li v zájmu jejich ekonomik užitečné rozšířit je do oblastí, na které se tato dohoda nevztahuje,
+SE ROZHODLY vzhledem k dosažení těchto cílů a s uvážením skutečnosti, že žádná ustanovení této dohody nelze vykládat jako zproštění závazků smluvních stran , které přijaly na základě jiných mezinárodních dohod,
+UZAVŘÍT TUTO DOHODU:
+Článek 1
+Cílem této dohody je
+a) podporovat rozšířením vzájemného obchodu harmonický rozvoj hospodářských vztahů mezi Evropským hospodářským společenstvím a Švýcarskou konfederací a přispívat tak ke zvyšování hospodářské činnosti, zlepšování životních podmínek a podmínek zaměstnanosti, růstu produktivity a finanční stabilitě ve Společenství a ve Švýcarsku,
+b) zajistit poctivé podmínky hospodářské soutěže při obchodu mezi smluvními stranami,
+c) přispět odstraněním obchodních překážek k harmonickému rozvoji a rozšíření světového obchodu.
+Článek 2
+Tato dohoda se použije na produkty pocházející ze Společenství a ze Švýcarska
+i) kapitol 25 až 99 Bruselské nomenklatury s výjimkou produktů uvedených v příloze I,
+ii) uvedené v protokolu č. 2 s patřičným ohledem na tento protokol.
+Článek 3
+1. V obchodu mezi Společenstvím a Švýcarskem se nezavedou žádná nová dovozní cla.
+2. Dovozní cla se postupně zruší podle tohoto časového plánu:
+a) dnem 1. dubna 1973 se všechna cla snižují na 80 % základního cla,
+b) další čtyři snížení cel, vždy o 20 %, se provedou
+1. ledna 1974,
+1. ledna 1975,
+1. ledna 1976,
+1. července 1977.
+Článek 4
+1. Ustanovení o postupném zrušení dovozních cel platí rovněž pro cla fiskální povahy.
+Smluvní strany mohou clo fiskální povahy nebo fiskální prvek cla nahradit vnitřní daní.
+2. Dánsko, Irsko, Norsko a Spojené království mohou ponechat clo fiskální povahy nebo fiskální prvek cla do 1. ledna 1976 při použití článku 38 aktu o podmínkách přistoupení a úpravách Smluv, vypracovaného a přijatého na konferenci mezi Evropskými společenstvími a Dánským královstvím, Irskem, Norským královstvím a Spojeným královstvím Velké Británie a Severního Irska.
+3. Při dodržení podmínek článku 18 může Švýcarsko dočasně ponechat cla odpovídající fiskálnímu prvku dovozního cla na produkty uvedené v příloze II.
+Smíšený výbor zřízený článkem 29 přezkoumá, zda jsou splněny podmínky stanovené v předchozím pododstavci, zejména pokud došlo ke změně v množství fiskálních prvků.
+Smíšený výbor přezkoumá situaci s ohledem na možnost přeměny takových cel na vnitřní poplatky před 1. lednem 1980 nebo před jiným dnem, které může podle okolností stanovit.
+Článek 5
+1. Základním clem, na které se vztahují postupná snižování stanovená v článku 3 a v protokolu č. 1, je pro všechny produkty clo skutečně používané dne 1. ledna 1972.
+2. Pokud po 1. lednu 1972 dojde ke snížením celních sazeb plynoucím z dohod uzavřených na základě obchodní konference konané v Ženevě v letech 1964 až 1967, nahradí takto snížená cla základní cla uvedená v odstavci 1.
+3. Snížená cla vypočtená podle článku 3 a protokolu č. 1 se používají zaokrouhlená na jedno desetinné místo.
+S výhradou použití čl. 39 odst. 5 aktu o podmínkách přistoupení a úpravách Smluv, vypracovaného a přijatého na konferenci mezi Evropskými společenstvími a Dánským královstvím, Irskem, Norským královstvím a Spojeným královstvím Velké Británie a Severního Irska, ze strany Společenství, pokud jde o zvláštní cla nebo zvláštní část smíšených cel v irském celním sazebníku, použijí se tento článek a protokol č. 1 se zaokrouhlením na čtyři desetinná místa.
+Článek 6
+1. V obchodu mezi Společenstvím a Švýcarskem se nezavedou žádné nové poplatky s účinkem rovnocenným dovoznímu clu.
+2. Poplatky s účinkem rovnocenným dovoznímu clu zavedené dne 1. ledna 1972 nebo po tomto dni v obchodu mezi Společenstvím a Švýcarskem se zrušují vstupem této dohody v platnost.
+Všechny poplatky s účinkem rovnocenným dovoznímu clu, jejichž výše dne 31. prosince 1972 je vyšší než poplatky skutečně používané dne 1. ledna 1972, se vstupem této dohody v platnost snižují na tuto nižší hodnotu.
+3. Poplatky s účinkem rovnocenným dovoznímu clu se postupně sníží podle tohoto časového plánu:
+a) nejpozději dne 1. ledna 1974 se všechny poplatky sníží na 60 % hodnoty používané dne 1. ledna 1972;
+b) další tři snížení, vždy o 20 %, se provedou
+1. ledna 1975,
+1. ledna 1976,
+1. července 1977.
+Článek 7
+1. V obchodu mezi Společenstvím a Švýcarskem se nezavedou žádná vývozní cla nebo poplatky s rovnocenným účinkem.
+Vývozní cla a poplatky s rovnocenným účinkem se zruší nejpozději dne 1. ledna 1974.
+2. U produktů uvedených v příloze III mohou smluvní strany způsobem, který samy určí, přijmout opatření, jež jsou nezbytná k provádění jejich dodavatelské politiky.
+Článek 8
+Protokol č. 1 stanoví celní režim a opatření pro některé produkty.
+Článek 9
+Protokol č. 2 stanoví celní režim a opatření pro některé zboží získané zpracováním zemědělských produktů.
+Článek 10
+1. V případě zavádění zvláštních pravidel v rámci zemědělské politiky nebo změny stávajících pravidel může dotčená smluvní strana upravit opatření vyplývající z této dohody s ohledem na produkty, které jsou předmětem těchto pravidel nebo změn.
+2. V těchto případech dotčená smluvní strana náležitě zohlední zájmy druhé smluvní strany. Za tímto účelem mohou smluvní strany spolu konzultovat v rámci smíšeného výboru.
